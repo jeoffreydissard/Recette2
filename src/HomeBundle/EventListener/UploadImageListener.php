d8f91a97c50107d08a1d6ff4529e2de50d3ede2d
jeoffrey@dissard.net <jeoffrey@dissard.net>
2018-11-27 15:32:42 +0100

    Image, Ajoutrecette, mesrecettes

---
+++ src/HomeBundle/EventListener/UploadImageListener.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace JournalBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use JournalBundle\Entity\Form;
+use JournalBundle\ImageUpload;
+
+class UploadImageListener
+{
+    private $uploader;
+
+    public function __construct(ImageUpload $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    private function uploadFile($entity)
+    {
+        // upload only works for Product entities
+        if (!$entity instanceof Form) {
+            return;
+        }
+
+        $file = $entity->getImage();
+
+        // only upload new files
+        if (!$file instanceof UploadedFile) {
+            return;
+        }
+
+        $fileName = $this->uploader->upload($file);
+        $entity->setImage($fileName);
+    }
+}
