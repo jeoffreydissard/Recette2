501ae3f05d90cf7e69ee4b2e1d4ed1124546dd9b
jeoffrey@dissard.net <jeoffrey@dissard.net>
2018-11-27 14:27:33 +0100

    Entity et FormType

---
+++ src/HomeBundle/Entity/Recette.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace HomeBundle\Entity;
+
+/**
+ * Recette
+ */
+class Recette
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nom;
+
+    /**
+     * @var string
+     */
+    private $photo;
+
+    /**
+     * @var string
+     */
+    private $categorie;
+
+    /**
+     * @var string
+     */
+    private $temps;
+
+    /**
+     * @var string
+     */
+    private $ingredients;
+
+    /**
+     * @var int
+     */
+    private $nbPersonnes;
+
+    /**
+     * @var string
+     */
+    private $preparation;
+
+    /**
+     * @var int
+     */
+    private $auteur;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Recette
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     *
+     * @return Recette
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Set categorie
+     *
+     * @param string $categorie
+     *
+     * @return Recette
+     */
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * Get categorie
+     *
+     * @return string
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * Set temps
+     *
+     * @param string $temps
+     *
+     * @return Recette
+     */
+    public function setTemps($temps)
+    {
+        $this->temps = $temps;
+
+        return $this;
+    }
+
+    /**
+     * Get temps
+     *
+     * @return string
+     */
+    public function getTemps()
+    {
+        return $this->temps;
+    }
+
+    /**
+     * Set ingredients
+     *
+     * @param string $ingredients
+     *
+     * @return Recette
+     */
+    public function setIngredients($ingredients)
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    /**
+     * Get ingredients
+     *
+     * @return string
+     */
+    public function getIngredients()
+    {
+        return $this->ingredients;
+    }
+
+    /**
+     * Set nbPersonnes
+     *
+     * @param integer $nbPersonnes
+     *
+     * @return Recette
+     */
+    public function setNbPersonnes($nbPersonnes)
+    {
+        $this->nbPersonnes = $nbPersonnes;
+
+        return $this;
+    }
+
+    /**
+     * Get nbPersonnes
+     *
+     * @return int
+     */
+    public function getNbPersonnes()
+    {
+        return $this->nbPersonnes;
+    }
+
+    /**
+     * Set preparation
+     *
+     * @param string $preparation
+     *
+     * @return Recette
+     */
+    public function setPreparation($preparation)
+    {
+        $this->preparation = $preparation;
+
+        return $this;
+    }
+
+    /**
+     * Get preparation
+     *
+     * @return string
+     */
+    public function getPreparation()
+    {
+        return $this->preparation;
+    }
+
+    /**
+     * Set auteur
+     *
+     * @param integer $auteur
+     *
+     * @return Recette
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur
+     *
+     * @return int
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+}
+
---
+++ src/HomeBundle/Form/RecetteType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace HomeBundle\Form;
+
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecetteType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom',     TextType::class)
+            ->add('photo', FileType::class, array('label' => 'insert_photo', 'label_attr' => array(
+                'class' => "material-icons  insertphoto"
+            ),'attr' => array(
+                'style' => "visibility: hidden;"
+            )))
+            ->add('categorie', ChoiceType::class, array(
+                'attr'  =>  array('class' => 'form-control',
+                    'style' => 'margin:5px 0;'),
+                'choices' =>
+                    array
+                    (
+                        'Test' => 'Test',
+                        'Web' => 'Web',
+                        'Voiture' => 'Voiture',
+                        'Moto' => 'Moto',
+                    ) ,
+                'multiple' => false,
+                'required' => true,
+            ))
+            ->add('temps',     TextType::class)
+            ->add('ingredients',     TextType::class)
+            ->add('nbPersonnes',     NumberType::class)
+            ->add('preparation',   TextareaType::class)
+            ->add('Ajouter', SubmitType::class,array('attr' => array(
+                'class' => "btn waves-effect waves-light cyan darken-3"
+            )));
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HomeBundle\Entity\Recette'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'homebundle_recette';
+    }
+
+
+}
